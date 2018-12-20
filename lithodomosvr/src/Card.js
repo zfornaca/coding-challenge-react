ccf81c87e8079fc8e6585c0ae0305d42505f53b5
Zack Fornaca <zfornaca@gmail.com>
2018-12-20 11:07:55 -0800

    images showing

--- lithodomosvr/src/Card.js
+++ lithodomosvr/src/Card.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
 class Card extends Component {
   render() {
-    const {name, scenes, id, image, tagline} = this.props;
+    const { name, scenes, id, image, tagline } = this.props;
     return (
-        <div className="Card">
-          <h3>{name}</h3>
-          {image ? <img src={image.file} alt=""/> : <div>no image</div>}
-          <div>{tagline}</div>        
-        </div>
+      <div className="Card">
+        <h3>{name}</h3>
+        {image ? (
+          <img src={`http://assets.lithodomosvr.com/${image.file}`} alt="" />
+        ) : (
+          <div>no image</div>
+        )}
+        <div>{tagline}</div>
+      </div>
     );
   }
 }
